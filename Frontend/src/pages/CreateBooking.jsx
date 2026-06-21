146efa259682b62c0cdc54334d7589cfcb1ffa2b
AnshPatel191207 <patelansh191207@gmail.com>
2026-06-21 11:14:10 +0530

    feat: connect booking forms fields validation

--- Frontend/src/pages/CreateBooking.jsx
+++ Frontend/src/pages/CreateBooking.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import * as Yup from 'yup';
+const validationSchema = Yup.object({
+  bookingId: Yup.string().required('Booking ID is required'),
+});
 export default function CreateBooking({ mode }) {
   return (
     <div>
